25931cc8eca52cd07b0cf4651c2f89465f19c085
sofiane <pendelliausofiane@gmail.com>
2025-02-04 12:35:04 +0100

    Ajout du read

--- index.php
+++ index.php
@@ -9,9 +9,28 @@
 </head>
 
 <body>
-<?php
+    <?php
     require_once("./dbconnect.php");
-?>  
+    if ($conn): ?>
+        <h1>Connexion à la BDD réussie!</h1>
+        <?php
+        $requete = "SELECT * FROM clubs";
+        $exec = $conn->query($requete);
+        $result = $exec->fetchAll(PDO::FETCH_ASSOC); ?>
+        <hr>
+        <?php foreach ($result as $key => $value):
+        ?>
+            <h2><?php echo $value["nom"]; ?></h2>
+            <ul>
+                <li><?php echo "<strong>Date</strong> : " . $value["creation"] ?></li>
+                <li><?php echo "<strong>Nombe de victoire Worlds</strong> : " . $value["v.champ"] ?></li>
+                <li><?php echo "<strong>Pays</strong> : " . $value["pays"] ?></li>
+            </ul>
+            <hr>
+    <?php
+        endforeach;
+    endif; ?>
 
 </body>
+
 </html>

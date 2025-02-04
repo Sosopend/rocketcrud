42b13f9d8639729ab43474b07d3e6c0ac3d999c3
karl <karl.clemence@outlook.fr>
2025-02-04 14:12:38 +0100

    modif index

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Base de données clubs</title>
-    <link rel="stylesheet" href="./style.css">
-</head>
-
-<body>
 <?php
+    require_once("./html_base/header.php");
     require_once("./dbconnect.php");
-    if($conn):?>
-        <h1>Connection à la BDD réussie!</h1>
+    if ($conn): ?>
+        <h1>Connexion à la BDD réussie!</h1>
+        <?php
+        $requete = "SELECT * FROM clubs";
+        $exec = $conn->query($requete);
+        $result = $exec->fetchAll(PDO::FETCH_ASSOC); ?>
+        <hr>
+        <?php foreach ($result as $key => $value):
+        ?>
+            <h2><?php echo $value["nom"]; ?></h2>
+            <ul>
+                <li><?php echo "<strong>Date</strong> : " . $value["creation"] ?></li>
+                <li><?php echo "<strong>Nombe de victoire Worlds</strong> : " . $value["v.champ"] ?></li>
+                <li><?php echo "<strong>Pays</strong> : " . $value["pays"] ?></li>
+            </ul>
+            <hr>
+    <?php
+        endforeach;
+        ?>
         <a href="http://cours-php.test/rocket/rocketcrud/form-ajout.php">Ajouter un club</a>
-        
-<?php endif;?>
 
-</body>
-</html>
+    <?php endif; 
+    require_once("./html_base/footer.php");
+    ?>
+

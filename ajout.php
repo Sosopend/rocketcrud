d87f1ef1710df06ffca0c2319ae8f0fcb5e3860e
sofiane <pendelliausofiane@gmail.com>
2025-02-12 15:48:12 +0100

    Ajout vérif nom club fonctionnel

--- ajout.php
+++ ajout.php
@@ -1,35 +1,41 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ajout à la BDD</title>
 </head>
+
 <body>
-<?php
+    <?php
     require_once("./dbconnect.php"); ?>
-<?php 
-    $nom = $_POST['nom'];
+    <?php
+    $nom = htmlspecialchars($_POST['nom']);
     $creation = $_POST['creation'];
     $vchamp = $_POST['vchamp'];
     $pays = $_POST['pays'];
-    if($conn):?>
+    $nomPattern = "/^[a-zA-Z0-9]{2,255}$/";
+    if ($conn): ?>
         <h1>Connection à la BDD réussie</h1>
 
-<?php // Requete d'ajout d'un produit
-        if (!empty($_POST["nom"]) && !empty($_POST["creation"]) && !empty($_POST["vchamp"]) && !empty($_POST["pays"])):
+    <?php 
+        if (!empty(trim($_POST["nom"])) && preg_match($nomPattern, $nom) && !empty($_POST["creation"]) && !empty($_POST["vchamp"]) && !empty($_POST["pays"])):
             $creation = (int)$_POST['creation'];
-            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES ('$nom', $creation, $vchamp, '$pays')";
-            // executer et stocker  la requête
-            $exec = $conn->query($requete);
-            if ($exec): header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
-                else: header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
-                endif; 
-            else:
-                header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
+            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES (:nom, $creation, $vchamp, '$pays')";
+         
+            $exec = $conn->prepare($requete);
+            $exec->bindParam('nom', $nom, PDO::PARAM_STR);
+            $result = $exec->execute();
+            if ($result): header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
+            else: header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
+            endif;
+        else:
+            header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
 
         endif;
-        endif; ?>
+    endif; ?>
 
 </body>
+
 </html>

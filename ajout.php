00e4b99d56945eacccd90c542098f0d1c3ccb473
karl <karl.clemence@outlook.fr>
2025-02-17 12:54:13 +0100

    commit vchamp

--- ajout.php
+++ ajout.php
@@ -12,39 +12,67 @@
     require_once("./dbconnect.php"); ?>
     <?php
     $nom = $_POST['nom'];
+    $errors = [];
     $creation = htmlspecialchars($_POST['creation']);
     $creapattern = "/^(200\d|201\d|202[0-5])$/";
+    $patternvchamp = "/^[0-9]\d*$/";
     $vchamp = $_POST['vchamp'];
     $pays = $_POST['pays'];
     if ($conn): ?>
         <h1>Connection à la BDD réussie</h1>
 
-    <?php // Requete d'ajout d'un produit
-        if (!empty($_POST["nom"]) && (isset($_POST['creation']) && !empty(trim($creation)) && preg_match($creapattern, $creation) && strlen($creation) == 4) && !empty($_POST["vchamp"]) && !empty($_POST["pays"])):
+    <?php
+        // Il faut tester toutes les conditions d'un coup puis faire du cas par cas
+        if (empty(trim($creation)) || !isset($creation) || !preg_match($creapattern, $creation) || strlen($creation) != 4):
+            $errors["creation"] = "";
+
+            if (empty(trim($creation))):
+                $errors["creation"] .= "le champs creation n'est pas renseigné";
+            endif;
+            if (!isset($creation)):
+                $errors["creation"] .= "le champs creation est vide";
+            endif;
+            if (!preg_match($creapattern, $creation)):
+                $errors["creation"] .= "le champs creation n'est pas valide";
+            endif;
+            if (strlen($creation) != 4):
+                $errors["creation"] .= "le champs creation doit contenir 4 chiffres";
+            endif;
+        endif;
+
+        if (empty(trim($vchamp)) || !isset($vchamp) || !preg_match($patternvchamp, $vchamp)):
+            $errors["vchamp"] = "";
+
+            if (empty(trim($vchamp))):
+                $errors["vchamp"] .= "le champs vchamp n'est pas renseigné";
+            endif;
+            if (!isset($vchamp)):
+                $errors["vchamp"] .= "le champs vchamp est vide";
+            endif;
+            if (!preg_match($patternvchamp, $vchamp)):
+                $errors["vchamp"] .= "le champs vchamp n'est pas valide";
+            endif;
+        endif;
+
+        if (empty($errors)):
             $creation = htmlspecialchars($_POST['creation']);
-            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES ('$nom', :creation, $vchamp, '$pays')";
+            $vchamp = htmlspecialchars($_POST['vchamp']);
+            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES ('$nom', :creation, :vchamp, '$pays')";
             //prepa de la requete
             $stmt = $conn->prepare($requete);
             //binder les valeurs
             $stmt->bindParam(':creation', $creation);
+            $stmt->bindParam(':vchamp', $vchamp);
             // executer et stocker  la requête
             $exec = $stmt->execute();
-            if ($exec): header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
-            else: header("Refresh: 0; URL=http://cours-php.test/rocket/rocketcrud/index.php");
-            endif;
+            header("Refresh: 5; URL=http://cours-php.test/rocket/rocketcrud/index.php");
         else:
-            if ($creation < 2000):
-                echo "Erreur!!";
-                echo "La date de création du Club doit être après 2000";
-            elseif ($creation > 2025):
-                echo "La date de création du Club doit être avant 2026";
-            else:
-                header("Refresh: 5; URL=http://cours-php.test/rocket/rocketcrud/index.php");
-
-            endif;
-
+            // gere une boucle foreach pour lister chaque erreur du tableau $errors
+            foreach ($errors as $key => $value):
+                echo $key . " " . $value;
+            endforeach;
         endif;
-    endif; ?>
+    endif;?>
 
 </body>
 

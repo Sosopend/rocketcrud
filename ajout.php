be765b17c7a3045dc2294f69939730d5a11818ca
karl <karl.clemence@outlook.fr>
2025-02-17 14:46:19 +0100

    ajout de pays en requête préparée

--- ajout.php
+++ ajout.php
@@ -18,28 +18,31 @@
     $creation = htmlspecialchars($_POST['creation']);
     $creapattern = "/^(200\d|201\d|202[0-5])$/";
     $patternvchamp = "/^[0-9]\d*$/";
-    $vchamp = $_POST['vchamp'];
-    $pays = $_POST['pays'];
+    $patternpays = "/^[A-Z]{1,3}$/";
+    $vchamp = htmlspecialchars($_POST['vchamp']);
+    $pays = htmlspecialchars($_POST['pays']);
     if ($conn): ?>
         <h1>Connection à la BDD réussie</h1>
 
     <?php
         // Il faut tester toutes les conditions d'un coup puis faire du cas par cas
         if (!isset($nom) || empty(trim($nom)) || !preg_match($nomPattern, $nom) || strlen($nom) < 2 || strlen($nom) > 255):
+            $errors["nom"] = "";
+
             if (!isset($nom)):
-                $errors["nom"] = "Le nom du club est obligatoire";
+                $errors["nom"] = "Le nom du club est obligatoire, ";
             endif;
             if (empty(trim($nom))):
-                $errors["nom"] = "Le nom du club ne peut pas être vide";
+                $errors["nom"] = "Le nom du club ne peut pas être vide, ";
             endif;
             if (!preg_match($nomPattern, $nom)):
-                $errors["nom"] = "Le nom du club n'est pas valide";
+                $errors["nom"] = "Le nom du club n'est pas valide, ";
             endif;
             if (strlen($nom) <= 1):
-                $errors["nom"] = "Le nom du club doit contenir au moins 2 caractères";
+                $errors["nom"] = "Le nom du club doit contenir au moins 2 caractère, ";
             endif;
             if (strlen($nom) >= 256):
-                $errors["nom"] = "Le nom du club ne peut pas contenir plus de 255 caractères";
+                $errors["nom"] = "Le nom du club ne peut pas contenir plus de 255 caractères, ";
             endif;
         endif;
 
@@ -47,16 +50,16 @@
             $errors["creation"] = "";
 
             if (empty(trim($creation))):
-                $errors["creation"] .= "le champs creation n'est pas renseigné";
+                $errors["creation"] .= "le champs creation n'est pas renseigné, ";
             endif;
             if (!isset($creation)):
-                $errors["creation"] .= "le champs creation est vide";
+                $errors["creation"] .= "le champs creation est vide, ";
             endif;
             if (!preg_match($creapattern, $creation)):
-                $errors["creation"] .= "le champs creation n'est pas valide";
+                $errors["creation"] .= "le champs creation n'est pas valide, ";
             endif;
             if (strlen($creation) != 4):
-                $errors["creation"] .= "le champs creation doit contenir 4 chiffres";
+                $errors["creation"] .= "le champs creation doit contenir 4 chiffres, ";
             endif;
         endif;
 
@@ -64,26 +67,50 @@
             $errors["vchamp"] = "";
 
             if (empty(trim($vchamp))):
-                $errors["vchamp"] .= "le champs vchamp n'est pas renseigné";
+                $errors["vchamp"] .= "le champs vchamp n'est pas renseigné, ";
             endif;
             if (!isset($vchamp)):
-                $errors["vchamp"] .= "le champs vchamp est vide";
+                $errors["vchamp"] .= "le champs vchamp est vide, ";
             endif;
             if (!preg_match($patternvchamp, $vchamp)):
-                $errors["vchamp"] .= "le champs vchamp n'est pas valide";
+                $errors["vchamp"] .= "le champs vchamp n'est pas valide, ";
             endif;
         endif;
+        if(!isset($pays) || empty(trim($pays)) || !preg_match($patternpays, $pays) || strlen($pays) < 3 || strlen($pays) > 3):
+            $errors["pays"] = "";
+            if (!isset($pays)):
+                $errors["pays"] .= "le champs pays est vide, ";
+                endif;
+            if (empty(trim($pays))):
+                $errors["pays"] .= "le champs pays n'est pas vide, ";
+                endif;
+            if (!preg_match($patternpays, $pays)):
+                $errors["pays"] .= "le champs pays n'est pas valide, ";
+                endif;
+            if (strlen($pays) < 3):
+                $errors["pays"] .= "le champs pays doit contenir 3 caractères, ";
+                endif;
+            if (strlen($pays) > 3):
+                $errors["pays"] .= "le champs pays doit contenir 3 caractères, ";
+                endif;
+            // verifier si uniquement en maj
+            if (preg_match('/[a-z]/', $pays)):
+                $errors["pays"] .= "le champs pays doit contenir que des MAJ";
+                endif;
+            endif;
+
 
         if (empty($errors)):
             $creation = htmlspecialchars($_POST['creation']);
             $vchamp = htmlspecialchars($_POST['vchamp']);
-            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES (:nom, :creation, :vchamp, '$pays')";
+            $requete = "INSERT INTO clubs (nom, creation, vchamp, pays) VALUES (:nom, :creation, :vchamp, :pays)";
             //prepa de la requete
             $stmt = $conn->prepare($requete);
             //binder les valeurs
             $stmt->bindParam(':creation', $creation);
             $stmt->bindParam(':vchamp', $vchamp);
             $stmt->bindParam(':nom', $nom);
+            $stmt->bindParam(':pays', $pays);
             // executer et stocker  la requête
             $exec = $stmt->execute();
             header("Refresh: 5; URL=http://cours-php.test/rocket/rocketcrud/index.php");
